92e15b041b051d95586e3812176e9db469792f17
BurningJump <935646729@qq.com>
2017-07-21 14:50:12 +0800

    first commit

---
+++ common_function.js
@@ -0,0 +1,25 @@
+//常用函数
+
+//鼠标移动速度，即每秒移动的像素数量
+function mouseMoveVelocity(){
+    var previousX;
+    var previousY;
+    var previousT;
+
+    window.addEventListener('mousemove', function(event) {
+        if (!(previousX === undefined ||
+              previousY === undefined ||
+              previousT === undefined)) {
+            var deltaX = event.screenX - previousX;
+            var deltaY = event.screenY - previousY;
+            var deltaD = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+        
+            var deltaT = event.timeStamp - previousT;
+            console.log(deltaD / deltaT * 1000);
+        }
+    
+        previousX = event.screenX;
+        previousY = event.screenY;
+        previousT = event.timeStamp;
+    });
+}
